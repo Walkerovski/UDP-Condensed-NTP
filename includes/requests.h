a47cf19d112492bd676cc75d56ae67f0f67a1f01
Walkerovski <barbardian@wp.pl>
2025-10-26 15:08:44 +0000

    Datagram Types

---
+++ includes/requests.h
@@ -0,0 +1,70 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+#include <cstdint>
+#include <vector>
+#include <array>
+#include <string>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+struct checksum_ctx;
+
+/**
+ * @brief Send a buffer over a socket.
+ *
+ * Attempts to send exactly @p size bytes from @p data using the socket
+ * file descriptor @p sockfd. If the number of bytes sent does not match
+ * the requested size, a std::runtime_error is thrown.
+ *
+ * @param sockfd   Socket file descriptor.
+ * @param data     Pointer to the buffer to send.
+ * @param size     Number of bytes to send.
+ * @param errMsg   Error message included in the exception if sending fails.
+ *
+ * @throws std::runtime_error if sending fails or fewer than @p size bytes are sent.
+ */
+void sendAny(int sockfd, const void* data, ssize_t size, const char* errMsg);
+
+/**
+ * @brief Receive a fixed-size buffer from a socket.
+ *
+ * Reads exactly @p size bytes into @p buffer from the socket @p sockfd.
+ * The function will loop until all bytes are received or an error occurs.
+ *
+ * @param sockfd   Socket file descriptor.
+ * @param buffer   Pointer to the buffer where received data will be stored.
+ * @param size     Number of bytes to receive.
+ * @param errMsg   Error message included in the exception if receiving fails.
+ *
+ * @throws std::runtime_error if the socket is closed or an error occurs.
+ */
+void receiveAny(int sockfd, void* buffer, ssize_t size, const char* errMsg);
+
+/* Protocol Structures */
+struct TimeRequest {
+    uint32_t sequence_number;
+    uint32_t version;
+    uint64_t client_seconds;
+    uint64_t client_nanoseconds;
+
+    void setValues(uint32_t seq_num, uint64_t client_sec, uint64_t client_nanosec, uint32_t ver=7);
+    void sendTo(int sockfd, sockaddr_in& client_addr) const;
+    void receive(int sockfd, sockaddr_in& client_addr);
+};
+
+struct TimeResponse {
+    uint32_t sequence_number;
+    uint32_t version;
+    uint64_t client_seconds;
+    uint64_t client_nanoseconds;
+    uint64_t server_seconds;
+    uint64_t server_nanoseconds;
+
+    void setValues(uint32_t seq_num, uint64_t client_sec, uint64_t client_nanosec, 
+                   uint64_t server_sec, uint64_t sever_nanosec, uint32_t ver=7);
+    void sendTo(int sockfd, sockaddr_in& client_addr) const;
+    void receive(int sockfd, sockaddr_in& client_addr);
+};
+
+#endif // REQUESTS_H
---
+++ src/requests.cpp
@@ -0,0 +1,90 @@
+#include "requests.h"
+
+#include <arpa/inet.h>
+#include <stdexcept>
+#include <algorithm>
+#include <cstdio>
+#include <iostream> 
+#include <cstring>
+#include <unistd.h>
+
+#define ERR_SEND(cls) ("Sending " #cls " failed!")
+#define ERR_RECV(cls) ("Receiving " #cls " failed!")
+
+using namespace std;
+
+uint64_t htonll(uint64_t value) {
+    return ((uint64_t)htonl(value & 0xFFFFFFFFULL) << 32) | htonl(value >> 32);
+}
+
+uint64_t ntohll(uint64_t value) {
+    return htonll(value);
+}
+
+
+void TimeRequest::setValues(uint32_t seq_num, uint64_t client_sec, uint64_t client_nanosec, uint32_t ver) {
+    sequence_number = htonl(seq_num);
+    version = htonl(ver);
+    client_seconds = htonll(client_sec);
+    client_nanoseconds = htonll(client_nanosec);
+}
+
+void TimeRequest::sendTo(int sockfd, sockaddr_in& addr) const {
+    ssize_t sent = sendto(sockfd, this, sizeof(*this), 0,
+                    (struct sockaddr*)&addr, sizeof(addr));
+    if (sent < 0) {
+        cerr << "sendto() failed: " << strerror(errno) << "\n";
+        close(sockfd);
+        return;
+    }
+}
+
+void TimeRequest::receive(int sockfd, sockaddr_in& addr) {
+    socklen_t addr_len = sizeof(addr);
+    ssize_t recv_len = recvfrom(sockfd, this, sizeof(*this), 0,
+                                (struct sockaddr*)&addr, &addr_len);
+    if (recv_len < 0) {
+        cerr << "recvfrom() failed: " << strerror(errno) << "\n";
+        return;
+    }
+    this->sequence_number = ntohl(this->sequence_number);
+    this->version = ntohl(this->version);
+    this->client_seconds = ntohll(this->client_seconds);
+    this->client_nanoseconds = ntohll(this->client_nanoseconds);
+}
+
+void TimeResponse::setValues(uint32_t seq_num, uint64_t client_sec, uint64_t client_nanosec,
+                             uint64_t server_sec, uint64_t sever_nanosec, uint32_t ver) {
+    sequence_number = htonl(seq_num);
+    version = htonl(ver);
+    client_seconds = htonll(client_sec);
+    client_nanoseconds = htonll(client_nanosec);
+    server_seconds = htonll(server_sec);
+    server_nanoseconds = htonll(sever_nanosec);
+}
+
+void TimeResponse::sendTo(int sockfd, sockaddr_in& addr) const {
+    ssize_t sent = sendto(sockfd, this, sizeof(*this), 0,
+                    (struct sockaddr*)&addr, sizeof(addr));
+    if (sent < 0) {
+        cerr << "sendto() failed: " << strerror(errno) << "\n";
+        close(sockfd);
+        return;
+    }
+}
+
+void TimeResponse::receive(int sockfd, sockaddr_in& addr) {
+    socklen_t addr_len = sizeof(addr);
+    ssize_t recv_len = recvfrom(sockfd, this, sizeof(*this), 0,
+                                (struct sockaddr*)&addr, &addr_len);
+    if (recv_len < 0) {
+        cerr << "recvfrom() failed: " << strerror(errno) << "\n";
+        return;
+    }
+    this->sequence_number = ntohl(this->sequence_number);
+    this->version = ntohl(this->version);
+    this->client_seconds = ntohll(this->client_seconds);
+    this->client_nanoseconds = ntohll(this->client_nanoseconds);
+    this->server_seconds = ntohll(this->server_seconds);
+    this->server_nanoseconds = ntohll(this->server_nanoseconds);
+}
