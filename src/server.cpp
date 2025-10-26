109ad95ba4e0a541521ed79069111c2155f39363
Walkerovski <barbardian@wp.pl>
2025-10-26 16:54:03 +0000

    Server Tracks Clients

--- src/server.cpp
+++ src/server.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 #include <arpa/inet.h>
 #include <time.h>
+#include <unordered_map>
 
 
 using namespace std;
@@ -32,6 +33,13 @@ bool initializeSocket(server_arguments& args, int sockfd) {
     return false;
 }
 
+string buildClientKey(sockaddr_in client_addr) {
+    char ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &(client_addr.sin_addr), ip, INET_ADDRSTRLEN);
+    uint16_t port = ntohs(client_addr.sin_port);
+    string key = string(ip) + ":" + to_string(port);
+    return key;
+}
 
 int main(int argc, char *argv[]) {
     server_arguments args{};
@@ -44,10 +52,27 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    unordered_map<string, pair<uint32_t, time_t>> client_map;
+    const int EXPIRATION = 120;
+
     while (true) {
         struct sockaddr_in client_addr;
         TimeRequest req{};
         req.receive(sockfd, client_addr);
+        time_t now = time(nullptr);
+        string key = buildClientKey(client_addr);
+        if (!client_map.count(key) || (now - client_map[key].second) > EXPIRATION) {
+            client_map[key] = {req.sequence_number, now};
+        } else {
+            uint32_t prev_max = client_map[key].first;
+
+            if (req.sequence_number < prev_max) {
+                cout << key << " " << req.sequence_number << " " << prev_max << "\n";
+            } else if (req.sequence_number > prev_max) {
+                client_map[key].first = req.sequence_number;
+            }
+            client_map[key].second = now;
+        }
 
         TimeResponse resp{};
         struct timespec ts;
@@ -59,6 +84,8 @@ int main(int argc, char *argv[]) {
             static_cast<uint64_t>(ts.tv_sec),
             static_cast<uint64_t>(ts.tv_nsec)
         );
-        resp.sendTo(sockfd, client_addr);
+        if (rand() % 100 >= args.drop_rate) {
+            resp.sendTo(sockfd, client_addr);
+        }
     }
 }
